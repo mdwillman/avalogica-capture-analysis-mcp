e7b6833971033ce4604c8a69d3423a32416f7b66
Marshall D. Willman <marshall.willman@protonmail.com>
2026-01-28 13:40:43 -0800

    Remove unused template tools

--- src/tools/index.ts
+++
@@ -1,6 +0,0 @@
-export { getTechUpdateTool } from "./getTechUpdate.js";
-export { listTechTopicsTool } from "./listTechTopics.js";
-export { searchEmergentSignalsTool } from "./searchEmergentSignals.js";
-export { searchEdgeCommunitiesTool } from "./searchEdgeCommunities.js";
-export { findSimilarPagesTool } from "./findSimilarPages.js";
-export { fetchPageContentsTool } from "./fetchPageContents.js";
